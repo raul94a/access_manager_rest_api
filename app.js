af99b2d3b1bcfd4bb1785190eef242540e97f199
raul.albin <raul200794@gmail.com>
2022-05-04 01:36:34 +0200

    User, AccessManagerDevice, AccessManagerDeviceData schemas and endpoint added

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const loadDatabase = require('./loaders/database-loader').initDatabase;
 const app = express();
 
+const authRoutes = require('./routes/auth');
+const accessManagerDeviceRoutes = require('./routes/accessManagerDevice')
 
 app.use(express.urlencoded({ extended: false }))
-//compulsory to set express.json() in order to listen to post request
+//Mandatory to set express.json() in order to listen to post request
 //in fact, what the statement does is to set the bodyparser to JSON format
 app.use(express.json());
 
+
+app.use(authRoutes.authRoutes);
+app.use(accessManagerDeviceRoutes.accessManagerDeviceRoutes);
 
 loadDatabase().then((_) => {
     const server = app.listen(3000);
